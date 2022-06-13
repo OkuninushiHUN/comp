2b5a7aeff3af09698d05d303e8d440be98323ac8
OkuninushiHUN <91747071+OkuninushiHUN@users.noreply.github.com>
2022-06-13 19:38:53 +0200

    Update competitions.php

--- app/Models/competitions.php
+++ app/Models/competitions.php
@@ -10,7 +10,7 @@ class competitions extends Model
 {
     use HasFactory,SoftDeletes;
     public function games(){
-        return $this->hasMany(Games::class);
+        return $this->belongsToMany(Games::class);
     }
     public function User(){
         return $this->belongsTo(User::class,'creator_id');
